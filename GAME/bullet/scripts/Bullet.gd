9cdfbf99d13e5d1b83d4df86566ead3e61250ad2
endcrs <end.cavalcant12@gmail.com>
2023-11-21 00:22:16 -0300

    Adicionando particulas de sangue

--- GAME/bullet/scripts/Bullet.gd
+++ GAME/bullet/scripts/Bullet.gd
@@ -1,12 +1,19 @@
 extends RigidBody2D
 
 var explosion = preload("res://bullet/Explosion.tscn")
+var blood = preload("res://zombie/particles/blood.tscn")
 
 func _on_Bullet_body_entered(body):
-	if !body.is_in_group("player"):
+	if !body.is_in_group("player") and !body.is_in_group("zombie"):
 		var explosion_instance = explosion.instance()
 		explosion_instance.position = global_position
 		get_tree().get_root().add_child(explosion_instance)
 		queue_free()
-	
-
+		
+	if body.is_in_group("zombie"):
+		var blood_instance = blood.instance()
+		blood_instance.position = global_position
+		blood_instance.rotation = global_rotation
+		blood_instance.emitting = true
+		get_tree().get_root().add_child(blood_instance)
+		queue_free()
--- GAME/zombie/scripts/zombie.gd
+++ GAME/zombie/scripts/zombie.gd
@@ -1,24 +1,30 @@
 extends KinematicBody2D
 
 var Life = 12
-var speed = 75
+var speed = 65
 
 var player_position = Vector2.ZERO
 var target_position = Vector2.ZERO
 
-export (int) var GRAVITY = 600
-
 var attacking = false
 var crounched = false
 
+export (int) var GRAVITY = 600
+
+onready var deathParticle = get_node("NodeZombie/Body/Neck/ExplosionHead")
 onready var player = get_parent().get_node("Player")
 onready var Head = get_node("NodeZombie/Body/Neck/Head")
 onready var ZombieAnimation = get_node("AnimationPlayer")
 
-func _ready():
-	Life = 12
-	$CollisionShapeZombie.disabled = false
+var blood = preload("res://zombie/particles/blood.tscn")
 
+func _ready():
+	# Vida do Inimigo
+	Life = 12
+	
+	# Evita que o Inimigo inicie sem colisão
+	$CollisionShapeZombie.disabled = false
+	
 func _physics_process(delta):
 	if (Life > 0):
 		if attacking:
@@ -53,26 +59,36 @@ func _set_flip():
 	else:
 		transform.x.x = 1
 
+func _blood(pos, rot):
+	var blood_instance = blood.instance()
+	blood_instance.position = pos
+	blood_instance.rotation = rot
+	blood_instance.emitting = true
+	get_tree().get_root().add_child(blood_instance)
+
 # Bullet acertou a cabeça
 func _on_Head_body_entered(body):
 	if (body.is_in_group("bullet")):
 		Life -= 4
+		_blood(body.position, body.rotation)
 		body.queue_free()
 		
 		if Life <= 0:
 			Head.visible = false
-	
+			deathParticle.emitting = true
 
 # Bullet acertou o corpo
 func _on_Body_body_entered(body):
 	if (body.is_in_group("bullet")):
 		Life -= 2
+		_blood(body.position, body.rotation)
 		body.queue_free()
 
 # Bullet acertou as pernas
 func _on_Legs_body_entered(body):
 	if (body.is_in_group("bullet")):
 		Life -= 1
+		_blood(body.position, body.rotation)
 		body.queue_free()
 		
 		if !crounched:
